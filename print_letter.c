8ce767c5aca0158681fd9b9845bef89119ad00fc
semaed <figsaned@gmail.com>
2023-03-28 00:42:07 +0000

    Add test function print letter

---
+++ print_letter.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_letter - function to be called
+ * @c: variable
+ * Return: 1
+ */
+
+int print_letter
+{
+	putchar(c);
+	return (1);
+}
