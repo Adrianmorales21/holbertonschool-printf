8a78bf38eb1d1211adc06cb6fcb4bae84322e09f
semaed <figsaned@gmail.com>
2023-03-29 19:24:11 +0000

    Add main headder file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_string(char *c);
+int print_letter(char c);
+int print_per(void);
+int print_int(int num);
+
+#endif
