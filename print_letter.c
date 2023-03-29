2ed62a63e198d0b003bd812317feed7b5854ee9d
semaed <figsaned@gmail.com>
2023-03-29 19:26:28 +0000

    Adding helper functions to print character, percentage symbol and strings

--- print_letter.c
+++ print_letter.c
@@ -8,7 +8,7 @@
  * Return: 1
  */
 
-int print_letter
+int print_letter(char c)
 {
 	putchar(c);
 	return (1);
---
+++ print_per.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_per - function to be called
+ * @n: variable
+ * Return:1
+ */
+
+int print_per(void)
+{
+	putchar('%');
+	return 1;
+}
