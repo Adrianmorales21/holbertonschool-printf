3cca4c03ca3276131dde905d22976f2c8475aed3
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-03-30 03:01:56 -0400

    fixes for auxiliar function andfor the printing integers function

--- print_int.c
+++ print_int.c
@@ -4,23 +4,19 @@ int print_int(int n)
     int digits = 0;
     int neg = 0;
 
-    if (n < 0)
-    {
+    if (n < 0) {
         neg = 1;
-        n *= -1;
+        _putchar('-');
+        digits++;
+        n = abs(n);
     }
 
     if (n / 10)
         digits += print_int(n / 10);
 
-    putchar(n % 10 + '0');
+    _putchar(n % 10 + '0');
     digits++;
 
-    if (neg)
-    {
-        putchar('-');
-        digits++;
-    }
-
-    return (digits);
+    return (digits + neg);
 }
+
