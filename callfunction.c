219a61fe8067efc22236056354aeb6832737c1ad
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-03-30 15:03:02 +0000

    fixes to the functions betty documentation and return in case of error

--- callfunction.c
+++ callfunction.c
@@ -1,45 +1,58 @@
 #include "main.h"
+/**
+ * call_functions - function to call the correct printing function.
+ * @format: pointer to the input
+ * @args: list of the arguments passed
+ * Return: pc if the function works correctly and -1 if it fails
+ */
 int call_functions(const char* format, va_list args)
 {
     int i, pc;
     pc = 0;
 
-    for (i = 0; format[i] != '\0'; i++) {
-        if (format[i] == '%') {
-            i++;
-            switch (format[i]) {
-              case 'c':{
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            _putchar(format[i]);
+            pc++;
+            continue;
+        }
+
+        i++;
+        switch (format[i]) {
+            case 'c':{
                 char c = va_arg(args, int);
                 _putchar(c);
                 pc ++;
                 break;
-              }
-              case 's':{
+            }
+            case 's':{
                 char *str = va_arg(args, char*);
                 pc += print_string(str);
                 break;
-              }
-		case '%':{
-		print_per();
-		pc++;
-		break;
-	      }
-                case 'd':
-                case 'i':{
-                                  int n = va_arg(args, int);
+            }
+            case '%':{
+                _putchar('%');
+                pc++;
+                break;
+            }
+            case 'd':
+            case 'i':{
+                int n = va_arg(args, int);
                 pc += print_int(n);
                 break;
             }
-                default:
-                    _putchar('%');
-                    _putchar(format[i]);
-                    pc += 2;
-                    break;
-            }
-        } else {
-            _putchar(format[i]);
-            pc++;
+            default:
+                _putchar('%');
+                _putchar(format[i]);
+                pc += 2;
+                break;
         }
+    }
+
+    if (pc <= 0) {
+        return -1; 
     }
 
     return pc;
